5f7d145083ae65f9b10c91462d36e9cb838b8348
Priyanka Koneru <koneru.priyanka@gmail.com>
2020-02-10 14:42:23 -0800

    Questions 2-4 answered

--- myquestions.js
+++ myquestions.js
@@ -6,13 +6,13 @@ const commitIdOne = "FILL_ME_IN";
 
 // 2) Modify myTrivia to contain the following with the given key names.
 /*
-  1. Who is your favorite super hero? key name: "favorite super hero"
-  2. What is one interesting thing about you? key name: "my thing"
-  3. What animal is your spirit animal? key name: "spirit animal"
-  4. What is your favorite number? key name: "favorite number"
-  5. How many reasons did you list above? key name: "my reasons"
-  6. What is your favorite place? key name: "favorite place"
-  7. What is your favorite movie? key name: "favorite movie"
+  1. Who is your favorite super hero? key name: "Tom Cruise"
+  2. What is one interesting thing about you? key name: "I am very emotional"
+  3. What animal is your spirit animal? key name: "Dog"
+  4. What is your favorite number? key name: "24"
+  5. How many reasons did you list above? key name: "Three"
+  6. What is your favorite place? key name: "Hawaii"
+  7. What is your favorite movie? key name: "Home Alone"
 */
 
 const myTrivia = {};
@@ -22,14 +22,14 @@ const commitIdTwo = "FILL_ME_IN";
 
 // 3) Finish the function that allows you to add a reason to myReasons array
 
-const addToMyReasons = () => {};
+const addToMyReasons = () => { myReasons.push('Career Goal') };
 
 // Add, Commit, Push, Verify
 const commitIdThree = "FILL_ME_IN";
 
 // 4) Finish the function that allows you to modify a given key value
 
-const addToMyTrivia = (key, value) => {};
+const addToMyTrivia = (key, value) => { myReasons[key] = value };
 
 // Advanced Content
 
