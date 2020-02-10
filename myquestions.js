3ab8d3ef4844a54b301c0454d8e2c84874fd9caa
JohnMichelin <john.michelin@galvanize.com>
2020-02-10 10:07:43 -0800

    Initial commit

---
+++ myquestions.js
@@ -0,0 +1,36 @@
+// 1) Add at least 3 reasons why you want to become a software developer
+const myReasons = [];
+
+// Add, Commit, Push, Verify
+const commitIdOne = "FILL_ME_IN";
+
+// 2) Modify myTrivia to contain the following with the given key names.
+/*
+  1. Who is your favorite super hero? key name: "favorite super hero"
+  2. What is one interesting thing about you? key name: "my thing"
+  3. What animal is your spirit animal? key name: "spirit animal"
+  4. What is your favorite number? key name: "favorite number"
+  5. How many reasons did you list above? key name: "my reasons"
+  6. What is your favorite place? key name: "favorite place"
+  7. What is your favorite movie? key name: "favorite movie"
+*/
+
+const myTrivia = {};
+
+// Add, Commit, Push, Verify
+const commitIdTwo = "FILL_ME_IN";
+
+// 3) Finish the function that allows you to add a reason to myReasons array
+
+const addToMyReasons = () => {};
+
+// Add, Commit, Push, Verify
+const commitIdThree = "FILL_ME_IN";
+
+// 4) Finish the function that allows you to modify a given key value
+
+const addToMyTrivia = (key, value) => {};
+
+// Advanced Content
+
+var passwordToParadise = "FILL_ME_IN";
